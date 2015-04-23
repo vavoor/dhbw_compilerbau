e31b8d61fba40e9ab8ae5b4029af8994f821e40a
Gerd Bleher <gerd.bleher@advantest.com>
2015-04-23 14:13:10 +0200

    Added implementation of symbol table

---
+++ pipifax/symtab.cpp
@@ -0,0 +1,44 @@
+#include "symtab.hpp"
+
+SymbolTable::SymbolTable()
+{
+  enterScope(); /* global scope always exists */
+}
+
+Node* SymbolTable::lookup(string* name)
+{
+  for (list<Scope*>::iterator l_it=m_symbols.begin(); l_it!=m_symbols.end(); l_it++) {
+    Scope* scope = *l_it;
+    Scope::iterator s_it = scope->find(name);
+    if (s_it!=scope->end()) {
+      return s_it->second;
+    }
+  }
+  return NULL;
+}
+
+bool SymbolTable::insert(string* name, Node* node)
+{
+  Scope* scope = m_symbols.front();
+  Scope::iterator it = scope->find(name);
+  if (it==scope->end()) {
+    (*scope)[name] = node;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void SymbolTable::enterScope()
+{
+  Scope* scope = new Scope();
+  m_symbols.push_front(scope);
+}
+
+void SymbolTable::leaveScope()
+{
+  Scope* scope = m_symbols.front();
+  m_symbols.pop_front();
+  delete scope;
+}
--- pipifax/symtab.hpp
+++ pipifax/symtab.hpp
@@ -1,6 +1,7 @@
 #ifndef ID8346d44d_bc8a_493e_915d_69920c31cce0
 #define ID8346d44d_bc8a_493e_915d_69920c31cce0
 
+#include <string>
 #include <list>
 #include <map>
 using namespace std;
@@ -10,10 +11,18 @@ class Node;
 class SymbolTable {
 private:
   typedef map<string*,Node*> Scope;
-  list<Scope> m_symbols;
+  list<Scope*> m_symbols;
 
 public:
-
+  SymbolTable();
+  
+  Node* lookup(string* name);
+  
+  /* Returns true if the element has been inserted */
+  bool insert(string* name, Node* node);
+  
+  void enterScope();
+  void leaveScope();
 };
 
 #endif
