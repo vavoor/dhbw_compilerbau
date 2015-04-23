a8a7ab261f7882e0455205984e78660155b7dfc6
Gerd Bleher <gerd.bleher@advantest.com>
2015-04-23 13:04:56 +0200

    Put symbol table in separate file. Collected include order dependencies in one header file.

---
+++ pipifax/parser_inc.hpp
@@ -0,0 +1,12 @@
+#ifndef IDe05afd8b_20ea_4b15_a468_54277c21d640
+#define IDe05afd8b_20ea_4b15_a468_54277c21d640
+
+/* This file makes sure that the automatically generates header files
+   along with the necessary definitions of data types are included in 
+   the right order.
+*/
+
+#include "ast.hpp"
+#include "parser.tab.h"
+
+#endif
